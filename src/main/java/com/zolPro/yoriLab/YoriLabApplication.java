26cc3e2ced335e42acd49eab8f4c61cae80b6779
zelo0 <minorSunbi@gmail.com>
2022-05-17 20:37:18 +0900

    initail push

---
+++ src/main/java/com/zolPro/yoriLab/YoriLabApplication.java
@@ -0,0 +1,13 @@
+package com.zolPro.yoriLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoriLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoriLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zolPro/yoriLab/YoriLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zolPro.yoriLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoriLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
