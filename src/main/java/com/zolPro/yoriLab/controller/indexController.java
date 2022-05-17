8e8f7042d1bd83dd3eb557b0333e0677ffa0b233
zelo0 <minorSunbi@gmail.com>
2022-05-17 21:48:06 +0900

    타임리프 기본 레이아웃 생성 - 부트스트랩 사용 가능

---
+++ src/main/java/com/zolPro/yoriLab/controller/indexController.java
@@ -0,0 +1,13 @@
+package com.zolPro.yoriLab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index(Model model) {
+        return "contents/index";
+    }
+}
