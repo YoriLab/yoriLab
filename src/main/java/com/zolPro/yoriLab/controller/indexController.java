d49200746fd2fadd1a7d72c95ff3ac316c10c786
zelo0 <minorSunbi@gmail.com>
2022-05-24 01:21:45 +0900

    추천 화면 경로를 컨트롤러에 추가

--- src/main/java/com/zolPro/yoriLab/controller/indexController.java
+++ src/main/java/com/zolPro/yoriLab/controller/indexController.java
@@ -1,13 +1,51 @@
 package com.zolPro.yoriLab.controller;
 
+import com.zolPro.yoriLab.domain.Food;
+import com.zolPro.yoriLab.domain.Recommendation;
+import com.zolPro.yoriLab.domain.WhenToCook;
+import com.zolPro.yoriLab.dto.RecommendationByDay;
+import com.zolPro.yoriLab.dto.RecommendationByWhen;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class indexController {
+
     @GetMapping("/")
     public String index(Model model) {
         return "contents/index";
+    }
+
+
+    @GetMapping("/recommendation")
+    public String recommendation(Model model) {
+        List<RecommendationByDay> recommendationFullList = new ArrayList<>();
+
+        // 임시 생성
+        Random random = new Random();
+        char randomChar = 'A';
+        for (int i = 0; i < 7; i++) { // 몇일차
+            RecommendationByDay recommendationByDay = new RecommendationByDay(i+1);
+            for (int j = 0; j < 3; j++) { // 아침 점심 저녁
+                RecommendationByWhen recommendationByWhen = new RecommendationByWhen(WhenToCook.values()[j]);
+                for (int k = 0; k < random.nextInt(5) + 1; k++) {
+                    Recommendation recommendation = new Recommendation(i + 1, WhenToCook.values()[j], new Date(),
+                            new Food("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnV_w7RbpYilhi7ifIg-DZcvZLscDM5ZDhPA&usqp=CAU", "음식 " + randomChar));
+                    randomChar++;
+                    recommendationByWhen.addRecomm(recommendation);
+                }
+                recommendationByDay.addRecommByWhen(recommendationByWhen);
+            }
+            recommendationFullList.add(recommendationByDay);
+        }
+
+        model.addAttribute("recommendationFullList", recommendationFullList);
+        return "contents/recommendation";
     }
 }
