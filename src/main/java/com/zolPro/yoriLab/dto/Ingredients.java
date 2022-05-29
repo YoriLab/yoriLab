d1165f17107e6254a01c068155a094700c341bed
rladudals1201 <kimyoungmin1201@gmail.com>
2022-05-29 14:54:28 +0900

    reciperecommend2 파일 수정, Ingredients 객체 추가

---
+++ src/main/java/com/zolPro/yoriLab/dto/Ingredients.java
@@ -0,0 +1,13 @@
+package com.zolPro.yoriLab.dto;
+
+public class Ingredients {
+    String name;
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
