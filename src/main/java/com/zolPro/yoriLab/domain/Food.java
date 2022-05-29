6054f0c4eead017dd9c22c7608504ffa74258daa
zelo0 <minorSunbi@gmail.com>
2022-05-29 14:33:18 +0900

    Ingredient 도메인 추가

--- src/main/java/com/zolPro/yoriLab/domain/Food.java
+++ src/main/java/com/zolPro/yoriLab/domain/Food.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @Data
 public class Food {
     private String imgUrl; // 음식 이미지 경로
     private String name; // 음식 이름
+    private List<Ingredient> ingredientList;
 }
---
+++ src/main/java/com/zolPro/yoriLab/domain/Ingredient.java
@@ -0,0 +1,11 @@
+package com.zolPro.yoriLab.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+public class Ingredient {
+    private String name;
+}
