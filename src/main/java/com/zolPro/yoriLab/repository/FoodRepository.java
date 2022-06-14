1197146a61ba3539b02e082e77e1438d6cee5b97
zelo0 <minorSunbi@gmail.com>
2022-06-14 17:27:59 +0900

    필요한 함수들 정의

--- src/main/java/com/zolPro/yoriLab/repository/FoodRepository.java
+++ src/main/java/com/zolPro/yoriLab/repository/FoodRepository.java
@@ -10,6 +10,17 @@ import java.util.List;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    @Query(value = "SELECT * from food f order by RAND() LIMIT :count", nativeQuery = true)
-    List<Food> findSpecificCount(@Param("count") Integer count);
+//        @Query(value = "SELECT f from Food f join fetch f.ingredientAmountList ia join fetch  ia.ingredient")
+//    @Query(value = "SELECT f from Food f order by rand() limit :count", nativeQuery = true)
+//    List<Food> findSpecificCount(@Param("count") Integer count);
+
+    @Query(value = "SELECT distinct f from Food f join fetch f.ingredientAmountList ia join fetch  ia.ingredient where f.id in :idList")
+    List<Food> findAllByIdList(@Param("idList") List<Long> idList);
+//    @Query(value = "SELECT f from Food f join fetch f.ingredientAmountList ia join fetch  ia.ingredient")
+//    List<Food> fetchIngredientOfFood(@Param("foodIdList") List<Long> foodIdList);
+
+    
+    // native query에서는 테이별명대로 - 즉, 처음 소문자
+    @Query(value = "select count(*) from food", nativeQuery = true)
+    Long countTotal();
 }
