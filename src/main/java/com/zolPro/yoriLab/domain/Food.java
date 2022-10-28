b80128ed2cddfb4e231f33ad3b860542d30907e0
zelo0 <minorSunbi@gmail.com>
2022-10-28 23:09:52 +0900

    food db 스키마 변경

--- src/main/java/com/zolPro/yoriLab/domain/Food.java
+++ src/main/java/com/zolPro/yoriLab/domain/Food.java
@@ -19,10 +19,21 @@ public class Food {
     private Long id;
     private String name; // 음식 이름
     private String imgUrl; // 음식 이미지 경로
+    private String way; // 조리 종류 (찌기, 굽기)
+    private String category; // 요리 종류 (반찬, 국)
+    @Lob
+    private String doc; // 조리법 글
+    // 영양소 양
+    private Double INFO_NA;
+    private Double INFO_WGT;
+    private Double INFO_PRO;
+    private Double INFO_FAT;
+    private Double INFO_CAR;
+    private Double INFO_ENG;
 
-    private Integer serving; // 몇 인분
-    private Integer cookTime; // 요리 시간
-    private Integer difficulty; // 난이도
+//    private Integer serving; // 몇 인분
+//    private Integer cookTime; // 요리 시간
+//    private Integer difficulty; // 난이도
 
 
     @OneToMany(mappedBy = "food")
